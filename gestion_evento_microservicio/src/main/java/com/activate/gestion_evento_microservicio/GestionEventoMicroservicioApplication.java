9c13badc6458755d641807a5f46685d777bd24b8
DSantiagG <davidgiron@unicauca.edu.co>
2024-10-08 14:26:37 -0500

    add initial project gestion_evento_microservicio

---
+++ gestion_evento_microservicio/src/main/java/com/activate/gestion_evento_microservicio/GestionEventoMicroservicioApplication.java
@@ -0,0 +1,13 @@
+package com.activate.gestion_evento_microservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEventoMicroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEventoMicroservicioApplication.class, args);
+	}
+
+}
---
+++ gestion_evento_microservicio/src/test/java/com/activate/gestion_evento_microservicio/GestionEventoMicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activate.gestion_evento_microservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEventoMicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
