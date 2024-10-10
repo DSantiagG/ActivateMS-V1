b90d024c0946951afcc5e947199132dd4d368499
Jeferson <jcastanoossa@unicauca.edu.co>
2024-10-09 22:43:54 -0500

    add publisher on notification queue

--- recommendation_ms/src/main/java/com/activate/ActivateMSV1/recommendation_ms/service/EventConsumerService.java
+++ recommendation_ms/src/main/java/com/activate/ActivateMSV1/recommendation_ms/service/EventConsumerService.java
@@ -1,12 +1,14 @@
 package com.activate.ActivateMSV1.recommendation_ms.service;
 
 import com.activate.ActivateMSV1.recommendation_ms.infra.DTO.EventInfoDTO;
+import com.activate.ActivateMSV1.recommendation_ms.infra.DTO.NotificationDTO;
 import com.activate.ActivateMSV1.recommendation_ms.infra.DTO.UserDTO;
 import com.activate.ActivateMSV1.recommendation_ms.infra.config.RabbitMQConfig;
 import com.activate.ActivateMSV1.recommendation_ms.infra.mappers.EventMapper;
 import com.activate.ActivateMSV1.recommendation_ms.infra.mappers.UserMapper;
 import com.activate.ActivateMSV1.recommendation_ms.infra.repository.EventRepository;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,9 @@ import org.springframework.stereotype.Service;
 public class EventConsumerService {
     @Autowired
     private EventRepository eventRepository;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
 
     @RabbitListener(queues = RabbitMQConfig.EVENT_QUEUE)
     public void receiveMessage(EventInfoDTO event) {
@@ -29,7 +34,11 @@ public class EventConsumerService {
         if(eventAlreadyExists) return;
 
         //TODO: Emparejarlo con los usuarios y enviarlo a notificaciones
+        NotificationDTO notification = new NotificationDTO();
+        notification.setUserId(1L);
+        notification.setMessage("Nuevo evento: " + event.getName());
 
 
+        rabbitTemplate.convertAndSend(RabbitMQConfig.NOTIFICATION_QUEUE, notification);
     }
 }
