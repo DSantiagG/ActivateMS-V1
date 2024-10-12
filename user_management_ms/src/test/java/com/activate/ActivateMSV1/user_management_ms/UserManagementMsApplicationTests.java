3ed243816db5c76dd0944040bfd6638625cc5652
Santiago Fernandez <dfernandezd@unicauca.edu.co>
2024-10-12 13:35:42 -0500

    Base structure

---
+++ user_management_ms/src/test/java/com/activate/ActivateMSV1/user_management_ms/UserManagementMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activate.ActivateMSV1.user_management_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
